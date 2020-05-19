249138d1509938d9a14c10072b7813307d62567e
Ali.NET <aliemami.net@gmail.com>
2020-05-19 13:32:32 +0430

    SS-0003: Change missing DB name

--- Epg/settings.py
+++ Epg/settings.py
@@ -87,7 +87,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': db_name,                      # Or path to database file if using sqlite3.
+        'NAME': 'epg',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
